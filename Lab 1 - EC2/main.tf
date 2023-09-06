f676d485a487bd63c928d1ca3361697c20b9ee86
chris wagner <cwagne17@students.towson.edu>
2023-09-06 18:21:41 -0400

    remove region inline comment

--- Lab 1 - EC2/main.tf
+++ Lab 1 - EC2/main.tf
@@ -2,20 +2,6 @@
 # Class: AIT 665 - Cloud Computing
 # Author: Christopher Wagner
 
-# The region us-east-1 is located in North Virginia.
-# It is the region closest to me and the one I deploy
-# to most often.
-#
-# An AWS region is a physical geographic location in the
-# world that contains a cluster of AWS data centers.
-# 
-# An AWS Availability Zone (AZ) is a distinct location 
-# within an AWS Region that are engineered to be isolated 
-# from failures in other Availability Zones. A single 
-# AWS Region contains multiple Availability Zones.
-# 
-# The us-east-1 region contains the following AZs:
-# us-east-1a, us-east-1b, us-east-1c, us-east-1d, us-east-1e
 provider "aws" {
   region = "us-east-1"
 }
